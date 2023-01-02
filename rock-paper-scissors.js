3f7f0fb0846874b5f8bfb4678e44d9ecd673b306
Josiah Schmedding <119763151+jschmedding@users.noreply.github.com>
2023-01-02 15:46:50 +0100

    repair playRound function which previously broke

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -19,66 +19,64 @@ if (randChoice === 0) {
 
 
 /* Play one round */
-let playerWin;
-let playerLose;
 let playerSelection;
-
 function playRound(playerSelection, computerChoice) {
-    
+    playerSelection = window.prompt('rock, paper, or scissors?')
     if (playerSelection.toLowerCase() === "rock" && computerChoice === "paper") {
-        return playerLose;
+        return console.log("playerLose1");
     } else if (playerSelection.toLowerCase() === "rock" && computerChoice === "scissors") {
-        return playerWin;
+        return console.log("playerWin1");
     } else if (playerSelection.toLowerCase() === "paper" && computerChoice === "rock") {
-        return playerWin;
+        return console.log("playerWin2");
     } else if (playerSelection.toLowerCase() === "paper" && computerChoice === "scissors") {
-        return playerLose;
+        return console.log("playerLose2");
     } else if (playerSelection.toLowerCase() === "scissors" && computerChoice === "paper") {
-        return playerWin;
+        return console.log("playerWin3");
     } else if (playerSelection.toLowerCase() === "scissors" && computerChoice === "rock") {
-        return playerLose;
+        return console.log("playerLose3");
     } else {
-        return "Draw!";
+        return console.log("Draw!");
     }
 }
-let win = 0;
-function roundWin() {
-    win++;
-    console.log("You win! " + playerSelection + " beats " + computerChoice + "!");
-}
-function roundLoss() {
-    console.log(`You lose! ${computerChoice} beats ${playerSelection.toLowerCase()}!`);
-}
-// console.log(playRound(playerSelection, computerChoice));
+playRound(playerSelection, computerChoice)
+// let win = 0;
+// function roundWin() {
+//     win++;
+//     console.log("You win! " + playerSelection + " beats " + computerChoice + "!");
+// }
+// function roundLoss() {
+//     console.log(`You lose! ${computerChoice} beats ${playerSelection.toLowerCase()}!`);
+// }
+// // console.log(playRound(playerSelection, computerChoice));
 
-// Game function
-function game() {
-    for (let i = 0; i < 5;) {
-        let playerSelection = window.prompt('rock, paper, or scissors?')
-        playRound(playerSelection, computerChoice);
-        if (playRound(playerSelection, computerChoice) === playerWin) {
-            i++;
-            return roundWin();
-        }
-        else if (playRound(playerSelection, computerChoice) === playerLose) {
-            i++;
-            return roundLoss();
-        }
-        else {
-            console.log("Draw!");
-        }
-        // if (playRound(playerSelection, computerChoice) == "Draw!") {
-        //     return [i--, console.log("Draw!")]
-        // }
-        // instead of decrement, try only incrementing on win/loss
-    }
-    if (i = 5 && win >= 3) {
-        console.log("You win the game!")
-    } else if (i = 5 && win < 3){
-        console.log("You lose the game!")
-    } else {
-        console.log("Time for the next round!")
-    }
-}
+// // Game function
+// function game() {
+//     for (let i = 0; i < 5;) {
+        
+//         playRound(playerSelection, computerChoice);
+//         if (playRound(playerSelection, computerChoice) === playerWin) {
+//             i++;
+//             roundWin();
+//         }
+//         else if (playRound(playerSelection, computerChoice) === playerLose) {
+//             i++;
+//             return roundLoss();
+//         }
+//         else {
+//             console.log("Draw!");
+//         }
+//         // if (playRound(playerSelection, computerChoice) == "Draw!") {
+//         //     return [i--, console.log("Draw!")]
+//         // }
+//         // instead of decrement, try only incrementing on win/loss
+//     }
+//     if (i = 5 && win >= 3) {
+//         console.log("You win the game!")
+//     } else if (i = 5 && win < 3){
+//         console.log("You lose the game!")
+//     } else {
+//         console.log("Time for the next round!")
+//     }
+// }
 
-game();
+// game();
