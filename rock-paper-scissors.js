11760c6056fc41c7aadd5ef27892f72bac7b93be
Josiah Schmedding <119763151+jschmedding@users.noreply.github.com>
2023-01-19 18:30:31 +0100

    Rewrite getComputerChoice() to return string
    getComputerChoice() was returning a number instead of a string, leading
    to issues in the game() function with the computerChoice variable not
    updating correctly.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,24 +1,27 @@
 
 /* Function to create computer's choice in one round */
 function getComputerChoice() {
-   return Math.floor(Math.random() * 3);
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    return randomChoice;
 }
-
-const randChoice = getComputerChoice();
-let computerChoice;
-if (randChoice === 0) {
-    computerChoice = "rock";
-} else if (randChoice === 1) {
-    computerChoice = "paper";
-} else {
-    computerChoice = "scissors";
-}
+let computerChoice = getComputerChoice();
+// const randChoice = getComputerChoice();
+// let computerChoice;
+// if (randChoice === 0) {
+//     computerChoice = "rock";
+// } else if (randChoice === 1) {
+//     computerChoice = "paper";
+// } else {
+//     computerChoice = "scissors";
+// }
 // Test function
 // getComputerChoice();
 // console.log(computerChoice);
+
 let win = 0;
 let loss = 0;
-let i = 0;
+
 /* Play one round */
 let playerSelection;
 
@@ -32,30 +35,31 @@ function playRound(playerSelection, computerChoice) {
     } else if (playerSelection.toLowerCase() === "paper" && computerChoice === "scissors") {
         return roundLoss();
     } else if (playerSelection.toLowerCase() === "scissors" && computerChoice === "paper") {
-        return roundWin();
+         return roundWin();
     } else if (playerSelection.toLowerCase() === "scissors" && computerChoice === "rock") {
-        return roundLoss();
+         return roundLoss();
     } else {
-        return console.log("Draw!");
+        console.log("Draw!");
     }
 }
 
 function roundWin() {
-    return [console.log(`You win! ${playerSelection}  beats  ${computerChoice}!`), win++]
+    [console.log(`You win! ${playerSelection}  beats  ${computerChoice}!`), win++]
 }
 function roundLoss() {
-    return [console.log(`You lose! ${computerChoice} beats ${playerSelection}!`), loss++];
+    [console.log(`You lose! ${computerChoice} beats ${playerSelection}!`), loss++];
 }
 // // console.log(playRound(playerSelection, computerChoice));
 
 // // Game function
 function game() {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++){
+        let computerChoice = getComputerChoice();
         getComputerChoice();
         playerSelection = window.prompt('rock, paper, or scissors?');
         playRound(playerSelection, computerChoice);
         }
-    if (i = 5 && win > 2) {
+    if (i = 5 && win > loss) {
        return console.log("You win the game!");
     } else {
        return console.log("You lose the game!");
